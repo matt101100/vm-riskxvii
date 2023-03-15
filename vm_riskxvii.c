687486cbeec693c8e8d57daf25dbcb30ba048486
matt <mlaz7837@uni.sydney.edu.au>
2023-03-15 02:43:37 +0000

    test

---
+++ vm_riskxvii.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "vm_memory.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Invalid number of arguments.\n");
+        return 1;
+    }
+}
