d2ea32bab33a66617d89e9e4498220085a944d6a
matt <mlaz7837@uni.sydney.edu.au>
2023-03-22 09:18:11 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     fclose(machine_instruction_ptr);
 
     for (int i = 0; i < MEMORY_SIZE; i++) {
-        printf("(i*4): %0x\n", vm.instruction_memory[i]);
+        printf("%d: %0x\n",i * 4, vm.instruction_memory[i]);
     }
 
     // main virtual machine loop
