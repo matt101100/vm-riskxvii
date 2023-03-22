d5a108544110cb0a11df19bf7267f0fd22521c59
matt <mlaz7837@uni.sydney.edu.au>
2023-03-22 06:09:16 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -636,7 +636,7 @@ int execute_lbu(uint32_t instruction, virtual_machine *vm) {
         
         default:
             // load the 32-bit value into target register
-            vm->registers[target] = (uint8_t)vm->data_memory[(vm->registers[source[0]] + immediate) / 4];
+            vm->registers[target] = vm->data_memory[(vm->registers[source[0]] + immediate) / 4];
     }
     vm->pc += 4;
     return 1;
