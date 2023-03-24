0a5ae46c69a297598c0c132afcf5440b7c757985
matt <mlaz7837@uni.sydney.edu.au>
2023-03-24 01:18:11 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -1099,7 +1099,7 @@ int execute_sb(uint32_t instruction, virtual_machine *vm) {
         
         default:
             // update requested data memory address
-            vm->memory[(vm->registers[source[0]] + immediate)] = (uint8_t)vm->registers[source[1]];
+            vm->memory[(vm->registers[source[0]] + immediate)] = vm->registers[source[1]];
             break;
     }
     vm->pc += 4;
