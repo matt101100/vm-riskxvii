b5cbb1b50a2e5d54878e664fd31d079885690ab9
matt <mlaz7837@uni.sydney.edu.au>
2023-03-24 02:39:03 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -551,7 +551,7 @@ int main(int argc, char *argv[]) {
                 // invalid
                 printf("Instruction not implemented: %x\n", instruction);
                 printf("PC: %x\n", vm.pc);
-                reg_dump(&vm);
+                register_dump(&vm);
                 return 1;
         }
         // printf("pc: %d\n", (vm.pc));
