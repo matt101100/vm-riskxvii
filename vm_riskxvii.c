d60ea2477581e7a61f6b3ca8997f3a513c8ac0d3
matt <mlaz7837@uni.sydney.edu.au>
2023-03-24 02:21:50 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -1243,7 +1243,7 @@ void execute_blt(uint32_t instruction, virtual_machine *vm) {
     uint8_t source[2];
     get_source_registers(instruction, SB, source);
 
-    if (vm->registers[source[0]] < vm->registers[source[1]]) {
+    if ((int32_t)vm->registers[source[0]] < (int32_t)vm->registers[source[1]]) {
         vm->pc += immediate;
         return;
     }
@@ -1267,7 +1267,7 @@ void execute_bge(uint32_t instruction, virtual_machine *vm) {
     uint8_t source[2];
     get_source_registers(instruction, SB, source);
 
-    if (vm->registers[source[0]] >= vm->registers[source[1]]) {
+    if ((int32_t)vm->registers[source[0]] >= (int32_t)vm->registers[source[1]]) {
         vm->pc += immediate;
         return;
     }
