73b38cd425b99be7508ba751698ef8038151b0f2
matt <mlaz7837@uni.sydney.edu.au>
2023-03-29 02:36:38 +0000

    s

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -792,6 +792,8 @@ int execute_store(uint32_t instruction, int instruction_label,
                  */
                 new_block.total_mem_size += 64;
             }
+
+            printf("block size: %d\n", new_block.total_mem_size);
             
             // update list of nodes with new block at the end
             if (vm->head == NULL) {
