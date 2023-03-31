8895f57bba19767687e87ef67b0367ae8f02bd27
matt <mlaz7837@uni.sydney.edu.au>
2023-03-31 03:47:27 +0000

    as

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -883,7 +883,7 @@ int execute_store(uint32_t instruction, int instruction_label,
                 vm->head = &new_block;
             } else {
                 block *current_node = vm->head;
-                while (current_node->next) {
+                while (current_node->next != NULL) {
                     current_node = current_node->next;
                 }
                 current_node->next = &new_block;
