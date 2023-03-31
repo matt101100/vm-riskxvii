cddec131b7082ef84d3b19c6c5ab346305ba2cc4
matt <mlaz7837@uni.sydney.edu.au>
2023-03-31 09:30:06 +0000

    as

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -248,7 +248,7 @@ uint8_t check_valid_heap_memory_access(uint32_t mem_address,
 
     block *temp = vm->head;
     uint32_t block_end_pointer = temp->mem_base_address + temp->usable_mem_size;
-    while (temp != NULL) {
+    while (block_end_pointer <= HEAP_SIZE + 0xb700) {
         block_end_pointer = temp->mem_base_address + temp->usable_mem_size;
 
         if (mem_address <= block_end_pointer) {
