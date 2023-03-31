6980f02bc9eec67c5d72254844c61ab80683f605
matt <mlaz7837@uni.sydney.edu.au>
2023-03-31 07:02:02 +0000

    as

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -241,7 +241,7 @@ uint8_t check_valid_heap_memory_access(uint32_t mem_address,
     uint32_t block_end_pointer = current_block->mem_base_address + current_block->usable_mem_size;
     while (current_block != NULL) {
         block_end_pointer = current_block->mem_base_address + current_block->usable_mem_size;
-        if (block_end_pointer > (0xb700 + HEAP_SIZE) - 1) {
+        if (block_end_pointer >= (0xb700 + HEAP_SIZE) - 1) {
             return 0;
         }
         if (mem_address <= block_end_pointer) {
